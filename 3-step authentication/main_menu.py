ea1c1e60d5bae6385a989e0d064aa04f8bfbc143
itspriya999 <98838794+itspriya999@users.noreply.github.com>
2024-06-30 22:01:07 +0530

    Add files via upload
    A three step authentication program for security purposes.

---
+++ 3-step authentication/main_menu.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import font
+import custom_button
+import garbled
+# import obscure
+# import password
+# import segments
+
+def load_garbled(window, menu_frame):
+    menu_frame.pack_forget()
+    garbled.start(window)
+    
+def start(win):
+    win.geometry("1280x600")
+    win.title("Graphical Authentication System")
+
+    menu_frame = Frame(win, height=600, width=1280)
+    menu_frame.pack(fill='both', expand=1)
+
+    label = Label(menu_frame, text="Graphical Authentication System", font=('Freestyle Script', 54))
+    label.pack(padx=40, pady=30)
+
+    btn_height = 90
+    btn_width = 450
+    btn_font = ('Trebuchet MS', 14)
+    
+    btn1 = custom_button.TkinterCustomButton(master=menu_frame, text="Test Garbled Images", text_font=btn_font,
+                                             height=btn_height, width=btn_width, corner_radius=10,
+                                             command=lambda: load_garbled(win, menu_frame)).place(relx=0.5, rely=0.5,
+                                                                                                  anchor=CENTER)
+    
+    win.mainloop()
+
+
+if __name__ == "__main__":
+    win = Tk()
+    start(win)
